ba90c82636a4f1f26e2d04fb0139dc3c0af19465
oscarcostas142-creator <oscarcostas142@gmail.com>
2026-07-28 23:32:52 +0800

    Create introImages.ts

---
+++ src/utils/introImages.ts
@@ -0,0 +1,6 @@
+export const INTRO_IMAGES = [
+  `${import.meta.env.BASE_URL}stamps/shou1.png`,
+  `${import.meta.env.BASE_URL}stamps/shou2.png`,
+  `${import.meta.env.BASE_URL}stamps/shou3.png`,
+  `${import.meta.env.BASE_URL}stamps/shou4.png`,
+];
